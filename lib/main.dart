31216b3ae62923c838e060ffda6d0c367bfba527
Galbac <zidan_2002@mail.ru>
2022-01-31 19:24:05 +0300

    lesson 43

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,98 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   // runApp(
+//   //   const Align(
+//   //     alignment: Alignment(-0.5, -0.9),
+//   //     child: Text('Салам алейкум',
+//   //         textDirection: TextDirection.ltr,
+//   //         style: TextStyle(
+//   //             color: Colors.tealAccent, // зеленый цвет текста
+//   //             fontSize: 26, // высота шрифта 26
+//   //             backgroundColor: Colors.grey) // черный цвет фона текста
+//   //         ),
+//   //   ),
+//   // );
+//   runApp(
+//       // Container(
+//       //   color: Colors.blueAccent,
+//       //   padding: const EdgeInsets.only(top: 100),
+//       //   constraints: const BoxConstraints.tightFor(width: 80, height: 80),
+//       //   alignment: Alignment.center,
+//       //   child: Container(
+//       //     color: Colors.orange,
+//       //     child: const Text('Быть любимым — это больше, чем быть богатым, потому что быть любимым означает быть счастливым. (Тилье К.)',
+//       //         textDirection: TextDirection.ltr,
+//       //         style: TextStyle(
+//       //             color: Colors.red, // зеленый цвет текста
+//       //             fontSize: 26, // высота шрифта 26
+//       //             backgroundColor: Colors.grey) // черный цвет фона текста
+//       //     ),
+//       //   ),
+//       // ),
+//
+//   //     Container(
+//   //   padding: const EdgeInsets.only(top: 100),
+//   //   alignment: Alignment.center,
+//   //   color: Colors.tealAccent,
+//   //
+//   //   child: Column(
+//   //     children: [
+//   //       Container(
+//   //         color: Colors.orange,
+//   //         constraints: const BoxConstraints.tightFor(width: 400),
+//   //         child: const Text(
+//   //           "'Быть любимым — это больше, чем быть богатым, потому что быть любимым означает быть счастливым. (Тилье К.)",
+//   //           textDirection: TextDirection.ltr,
+//   //           textAlign: TextAlign.center,
+//   //           style: TextStyle(
+//   //               color: Colors.black, fontSize: 26, backgroundColor: Colors.white70),
+//   //         ),
+//   //       ),
+//   //       Container(
+//   //         color: Colors.red,
+//   //         constraints: const BoxConstraints.tightFor(width: 400),
+//   //         child: const Text(
+//   //           "'Быть любимым — это больше, чем быть богатым, потому что быть любимым означает быть счастливым. (Тилье К.)",
+//   //           textDirection: TextDirection.ltr,
+//   //           textAlign: TextAlign.center,
+//   //           style: TextStyle(
+//   //               color: Colors.black, fontSize: 26, backgroundColor: Colors.white70),
+//   //         ),
+//   //       ),
+//   //     ],
+//   //   ),
+//   // ));
+//
+//
+//   //   //Не заработал код
+//   //     Center(
+//   //       child: Container(
+//   //         padding: EdgeInsets.all(30),
+//   //         color: Colors.teal,
+//   //         child: const Icon(Icons.mobile_screen_share, size: 100,)),
+//   //     ),
+//   // );
+// }
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const Text('Cалам, алейкум' ,
-  textDirection: TextDirection.ltr ,
-  textAlign:TextAlign.center,
-    style: TextStyle(
-      bool inherit = 
-    ),
-  ),
-  );
+void main(){
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SZZ',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'Zhizn'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter +=1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ],
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Icon(
+            Icons.ac_unit,
+            size: 200,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add_alert),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
